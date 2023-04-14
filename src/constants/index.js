272d09ab9fec37021f05b2742729f984d1762c0a
deva961 <rajeshreddy.allala1@gmail.com>
2023-04-14 13:33:22 -0400

    14-04-2023
    Signed-off-by: deva961 <rajeshreddy.allala1@gmail.com>

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const sliders = [
+  {
+    desktop: require("../assets/images/mobile_images/desktop_slider_1.webp"),
+    tab: require("../assets/images/mobile_images/tab_slider_1.webp"),
+    sm: require("../assets/images/mobile_images/sm_slider_1.webp"),
+    mobile: require("../assets/images/mobile_images/mobile_slider_1.webp"),
+  },
+  {
+    desktop: require("../assets/images/mobile_images/desktop_slider_1.webp"),
+    tab: require("../assets/images/mobile_images/tab_slider_1.webp"),
+    sm: require("../assets/images/mobile_images/sm_slider_1.webp"),
+    mobile: require("../assets/images/mobile_images/mobile_slider_1.webp"),
+  },
+  {
+    desktop: require("../assets/images/mobile_images/desktop_slider_1.webp"),
+    tab: require("../assets/images/mobile_images/tab_slider_1.webp"),
+    sm: require("../assets/images/mobile_images/sm_slider_1.webp"),
+    mobile: require("../assets/images/mobile_images/mobile_slider_1.webp"),
+  },
+  {
+    desktop: require("../assets/images/mobile_images/desktop_slider_1.webp"),
+    tab: require("../assets/images/mobile_images/tab_slider_1.webp"),
+    sm: require("../assets/images/mobile_images/sm_slider_1.webp"),
+    mobile: require("../assets/images/mobile_images/mobile_slider_1.webp"),
+  },
+];
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4vee6pYCvhfm6x-HYkJmXfNsVKGsmuPk",
+  authDomain: "arkarenault-277a7.firebaseapp.com",
+  projectId: "arkarenault-277a7",
+  storageBucket: "arkarenault-277a7.appspot.com",
+  messagingSenderId: "766748457861",
+  appId: "1:766748457861:web:39d2bd4b818e3ecac9a2ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
